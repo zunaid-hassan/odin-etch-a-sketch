065095f532fe1611fd86199739a8523fb97cde00
zunaid.hassan <67002737+zunaid-hassan@users.noreply.github.com>
2025-11-19 20:49:03 +0800

    Finish main project objectives

--- script.js
+++ script.js
@@ -1,20 +1,66 @@
-const gridNumber = Number(prompt("Enter grid number between 1-100: "));
-// gridNumber = 5;
-let gridContainer = document.querySelector(".grid-container");
+log = console.log;
+
+gridQtBtn = document.querySelector("#grid-quantity-button");
+gridQtBtn.addEventListener("click", setGridQuantity);
+
+// gridSzBtn = document.querySelector("#grid-container-size-button");
+// gridSzBtn.addEventListener("click", setGridSize);
+
+const gridContainer = document.querySelector(".grid-container");
 gridContainer.addEventListener("mouseover", colorGrid);
 
-function createGrids() {
-  if (gridNumber > 0 && gridNumber <= 100) {
-    let gridContainer = document.querySelector(".grid-container");
-    gridContainer.addEventListener("mouseover", colorGrid);
-    for (let i = 0; i < gridNumber; i++) {
-      const grid = document.createElement("div");
-      grid.classList = "grid";
-      gridContainer.appendChild(grid);
+function createDefaultGrid() {
+  const nValue = 16;
+  const dimension = nValue * nValue;
+  gridWidth = gridContainer.clientWidth / nValue + "px";
+  gridHeight = gridContainer.clientHeight / nValue + "px";
+  for (let i = 0; i < dimension; i++) {
+    const gridCell = document.createElement("div");
+    gridCell.classList = "grid-cell";
+    gridCell.style.width = gridWidth;
+    gridCell.style.height = gridHeight;
+    gridContainer.appendChild(gridCell);
+  }
+}
+createDefaultGrid();
+
+let gridNumber;
+function setGridQuantity() {
+  while (true) {
+    const input = prompt("Enter a value for n for n×n grid (1-100): ");
+    if (input === null) {
+      return;
     }
-  } else {
-    alert("Sorry, refresh and give a number between 1 and 100!");
-    return;
+
+    gridNumber = Number(input);
+    if (isNaN(gridNumber) || gridNumber < 1 || gridNumber > 100) {
+      alert("Invalid input, give a number between 1 and 100.");
+    } else {
+      createGrids(gridNumber);
+      return;
+    }
+  }
+}
+
+function createGrids(gridNumber) {
+  log(gridNumber);
+  clearGrid();
+  gridWidth = gridContainer.clientWidth / gridNumber + "px";
+  gridHeight = gridContainer.clientHeight / gridNumber + "px";
+  dimension = gridNumber * gridNumber;
+  for (let i = 0; i < dimension; i++) {
+    const gridCell = document.createElement("div");
+    gridCell.classList = "grid-cell";
+    gridCell.style.width = gridWidth;
+    gridCell.style.height = gridHeight;
+    gridContainer.appendChild(gridCell);
+  }
+}
+
+function clearGrid() {
+  gridList = document.querySelectorAll(".grid-cell");
+  while (gridContainer.firstChild) {
+    gridContainer.removeChild(gridContainer.firstChild);
   }
 }
 
@@ -22,7 +68,7 @@ function colorGrid(event) {
   const target = event.target;
   const id = target.id;
 
-  if (!target.classList.contains("grid")) {
+  if (!target.classList.contains("grid-cell")) {
     return;
   }
   // Set colour change transition duration
@@ -33,8 +79,6 @@ function colorGrid(event) {
   let g = Math.ceil(256 * Math.random());
   let b = Math.ceil(256 * Math.random());
 
-  target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-  //   target.style.backgroundColor = `slategray`;
+  //   target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+  target.style.backgroundColor = `lightgray`;
 }
-
-createGrids();
